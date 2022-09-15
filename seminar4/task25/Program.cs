323dc82880db3ab476201ac9d53532e6e449dce7
Azamat <azamat-nazmutdinov@yandex.ru>
2022-09-15 22:18:13 +0500

    realized all seminar tasks

---
+++ seminar4/task25/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+// Не использовать Math.Pow() и аналоги!
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int ProdNumbersFrom1ToAdegB(int a, int b)
+{
+    if (a >= 1 && b >= 1) //если А и B больше либо равно 1, то работаем дальше
+    {
+        int multi = 1;
+        for (int i = 1; i <= b; i++)
+        {
+            multi *= a; //копим произведение
+        }
+        return multi; //вернем накопившееся произведение
+    }
+    else
+    {
+        Console.WriteLine("Числа A и B должны быть не меньше 1!");
+        return -1; //неправильная сумма (код ошибки)
+    }
+}
+
+int numberA = ReadNumber("Введите число:");
+int numberB = ReadNumber("Введите натуральную степень:");
+int degree = ProdNumbersFrom1ToAdegB(numberA, numberB);
+Console.WriteLine($"Число A в степени B = {degree}");
---
+++ seminar4/task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetCountOfDigits(int num)
+{
+    int sum = 0; //сумма цифр
+    while(num != 0)
+    {
+        sum += num%10;
+        num = num / 10;  
+    }
+    return sum;
+}
+
+int number = ReadNumber("Введите число:");
+int sum = GetCountOfDigits(number);
+Console.WriteLine(sum);
