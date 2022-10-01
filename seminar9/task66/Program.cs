9ac814e219fece7c9b1139ca9d87c4183ef63f89
Azamat <azamat-nazmutdinov@yandex.ru>
2022-10-01 17:42:30 +0500

    realized all seminar tasks

---
+++ seminar9/task66/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int ReadNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void SumMtoN(int m, int n, int sum)
+{
+    if(n < m)
+    {
+        sum += m;
+        SumMtoN(m - 1, n, sum);
+    }
+    else if(n > m)
+    {
+        sum += n;
+        SumMtoN(m, n - 1, sum);
+    }
+    else if(n == m || (m == n - 1) || (n == m - 1)) 
+    {
+        sum += n;
+        Console.WriteLine($"Сумма натуральных чисел в промежутке от M до N: {sum}");
+    }
+}
+
+int M = ReadNumber("Введите M");
+int N = ReadNumber("Введите N");
+int summa = 0;
+SumMtoN(M, N, summa);
---
+++ seminar9/task68/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadNumber(string message)
+{
+    int number = -1;
+    while(number < 0)
+    {
+        Console.WriteLine(message);
+        number = Convert.ToInt32(Console.ReadLine());
+    }
+    return number;
+}
+
+int FunctionsAckerman(int n, int m)
+{
+    if (n == 0)
+    {
+        return m + 1;
+    }
+    if (m == 0)
+    {
+        return FunctionsAckerman(n - 1, 1);
+    }
+    return FunctionsAckerman(n - 1, FunctionsAckerman(n, m - 1));
+}
+
+int M = ReadNumber("Введите неотрицательное число M");
+int N = ReadNumber("Введите неотрицательное число N");
+Console.WriteLine($"A({M},{N}) = {FunctionsAckerman(M, N)}");
